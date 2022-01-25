f8a55c842419828f87b75be0c6786727aa61b018
Zohaibkhattak15 <zohaibkhattak6@gmail.com>
2022-01-25 14:42:27 +0500

    commmit on main

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import { FC, useReducer } from "react";
 import "./App.css";
-import Navbar from "./components/Navbar/Navbar";
 import {
   Reducer,
   initialState,
   EmployeeContext,
 } from "./context/EmployeeContext";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Login from "./components/Login/Login";
 
 const App: FC = () => {
   const [state, dispatch] = useReducer(Reducer, initialState);
     return (
     <div className="App">
       <EmployeeContext.Provider value={{ state, dispatch }}>
+        <Router>
+           <Routes>
+             <Route path="/" element={<Login /> } />
+           </Routes>
+        </Router>
       </EmployeeContext.Provider>
     </div>
   );
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,41 +1,52 @@
-import { Form, Input ,Button } from "antd";
-import { FC } from "react";
+import { Form, Input, Button } from "antd";
+import { FC, useEffect, useState } from "react";
+import { getData } from "../../utils/apiUtils";
 import { Section, Wrapper } from "./style";
 
 const Login: FC = () => {
+  const [data, setData] = useState<any>();
+
+  const getDataRecord = async () => {
+    const response = await getData().then((data) => data);
+    setData(response);
+  };
+
+  useEffect(() => {
+    getDataRecord();
+  }, []);
+
   return (
     <Wrapper>
-    <Section>
-      <Form
-        name="basic"
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        initialValues={{}}
-        autoComplete="off"
-      >
-        <Form.Item
-          label="Username"
-          name="username"
-          rules={[{ required: true, message: "Please input your email!" }]}
+      <Section>
+        <Form
+          name="basic"
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 16 }}
+          initialValues={{}}
+          autoComplete="off"
         >
-          <Input placeholder="Email"/>
-        </Form.Item>
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[{ required: true, message: "Please input your email!" }]}
+          >
+            <Input placeholder="Email" />
+          </Form.Item>
 
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: "Please input your password!" }]}
-        >
-          <Input.Password placeholder="Pin"/>
-        </Form.Item>
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-        <Button type="primary" htmlType="submit">
-          Login
-        </Button>
-      </Form.Item>
-
-      </Form>
-    </Section>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password placeholder="Pin" />
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button type="primary" htmlType="submit">
+              Login
+            </Button>
+          </Form.Item>
+        </Form>
+      </Section>
     </Wrapper>
   );
 };
